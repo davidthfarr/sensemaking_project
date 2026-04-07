e140cd1273b4a9076bd1658be5c4e15a442517bc
Joey Schafer <schaferj@cs.washington.edu>
2026-04-07 13:14:51 -0700

    Reconstructed Post schema

---
+++ sensemaking/data/schemas.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+class Post:
+    def __init__(self, post_id: str | int, user_id: str | int, timestamp: datetime, text: str, embedding = None, stance = None, is_noise=False):
+        if not isinstance(post_id, str) and not isinstance(post_id, int):
+            raise TypeError(f"Expected 'post_id' to be str or int, got {type(post_id).__name__}")
+        if not isinstance(user_id, int) and not isinstance(user_id, str):
+            raise TypeError(f"Expected 'user_id' to be str or int, got {type(user_id).__name__}")
+        if not isinstance(timestamp, str) and not isinstance(timestamp, datetime):
+            raise TypeError(f"expected 'timestamp' to be a str or timestamp, got {type(timestamp).__name__}")
+        if not isinstance(text, str):
+            raise TypeError(f"expected 'text' to be str, got {type(text).__name__}")
+
+        self.post_id = post_id
+        self.user_id = user_id
+        self.timestamp = timestamp
+        self.text = text
+
+        self.stance = stance
+        self.embedding = embedding
+        self.is_noise = is_noise
